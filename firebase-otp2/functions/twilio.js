68e40870f743df1b288bd184f272de9c09469030
kaskrex <41402443+kaskrex@users.noreply.github.com>
2019-03-21 19:09:49 +0800

    Update twilio.js

--- firebase-otp2/functions/twilio.js
+++ firebase-otp2/functions/twilio.js
@@ -1,5 +1,5 @@
 const twilio  = require('twilio');
-const accountSid = 'AC962897a414d64dcc4d047c460286ef57';
-const authToken = '2674a25f2e9d63088815788e4e144ae8';
+const accountSid = '';
+const authToken = '';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
